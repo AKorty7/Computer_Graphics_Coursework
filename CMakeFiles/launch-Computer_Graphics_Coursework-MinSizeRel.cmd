ef58c9b79841f186c8a41cd8b950fd6383494b6d
AKorty7 <76076640+AKorty7@users.noreply.github.com>
2025-05-15 23:48:33 +0100

    Add project files.

---
+++ CMakeFiles/launch-Computer_Graphics_Coursework-MinSizeRel.cmd
@@ -0,0 +1,9 @@
+cd E:/Graphics Course work/source/
+set 
+
+if [%1]==[--debugger] (
+	SHIFT
+)
+
+"E:\Graphics Course work\build\MinSizeRel\Computer_Graphics_Coursework.exe"  
+pause
---
+++ CMakeFiles/launch-Computer_Graphics_Coursework-RelWithDebInfo.cmd
@@ -0,0 +1,9 @@
+cd E:/Graphics Course work/source/
+set 
+
+if [%1]==[--debugger] (
+	SHIFT
+)
+
+"E:\Graphics Course work\build\RelWithDebInfo\Computer_Graphics_Coursework.exe"  
+pause
