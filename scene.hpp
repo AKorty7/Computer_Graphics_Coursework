4fdeb735ef0531d68cbb406db9364e210f5fa9df
AKorty7 <76076640+AKorty7@users.noreply.github.com>
2025-05-16 23:21:50 +0100

    Added src/scene.hpp to define Scene class for park scene with ground, benches, trees. Uses common/maths.hpp for transformations. See src/scene.hpp, lines 10-30.

---
+++ scene.hpp
@@ -0,0 +1,29 @@
+#ifndef SCENE_HPP
+#define SCENE_HPP
+
+#include <GL/glew.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <common/shader.hpp>
+#include <common/maths.hpp>
+#include <vector>
+
+class Scene {
+private:
+    GLuint groundVAO, groundVBO;
+    GLuint benchVAO, benchVBO;
+    GLuint treeVAO, treeVBO;
+    std::vector<glm::mat4> benchModels;
+    std::vector<glm::mat4> treeModels;
+
+    void initGround();
+    void initBenches();
+    void initTrees();
+
+public:
+    Scene();
+    ~Scene();
+    void render(Shader& shader);
+};
+
+#endif
